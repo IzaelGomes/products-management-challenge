07d5bece8fe1ba9f354eb9206f392b2f65ae069c
Izael gomes <izaelgomes127@gmail.com>
2025-02-03 11:29:58 -0300

    refactor: header positions

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -10,30 +10,30 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="flex justify-between items-center bg-black mb-5 p-3 w-[100%]">
+    <header className="bg-black mb-5 p-3 w-[100%]">
       <SignedIn>
-        <Link className="text-white" href={"/"}>
-          Home
-        </Link>
-      </SignedIn>
-      <SignedIn>
-        <div className="flex justify-center items-center gap-4">
-          <Link href={"/product/create"} className="text-white">
-            Criar Produto
+        <div className="flex justify-between items-center gap-4">
+          <Link className="text-white" href={"/"}>
+            Home
           </Link>
-          <UserButton />
+
+          <div className="flex justify-center items-center gap-4">
+            <Link href={"/product/create"} className="text-white">
+              Criar Produto
+            </Link>
+            <UserButton />
+          </div>
         </div>
       </SignedIn>
+
       <SignedOut>
-        <div className="flex gap-5">
+        <div className="flex justify-end gap-4">
           <SignInButton>
             <Button>Entrar</Button>
           </SignInButton>
           <SignUpButton>
             <Button>Criar conta</Button>
           </SignUpButton>
-
-          <Link href={"/products"}>Produtos</Link>
         </div>
       </SignedOut>
     </header>
