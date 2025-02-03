e3b902d66940a961d2fc393c76466a73d7d63a0e
Izael gomes <izaelgomes127@gmail.com>
2025-02-03 10:04:25 -0300

    feat: not found page global

---
+++ web/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-7xl">404</h1>
+      <p className="text-center">
+        A página que você está buscando não foi encontrada!
+      </p>
+    </div>
+  );
+}
